20c5d8df6913c9039b523a852f8e5a2132b86740
cezaryrk <cezaryrk@361063.no-reply.drupal.org>
2012-02-06 00:18:07 +0100

    add table template

---
+++ field-group-multiple-table.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * - group
+ * - items
+ *  - delta + field_name + value render_array or null if empty
+ */
+?>
+<div class="field clearfix field-label-inline">
+
+<div class="field-label"><?= $group->label ?></div>
+
+<div class="field-items-table">
+<?= $table ?>
+</div>
+</div>
+</div>
