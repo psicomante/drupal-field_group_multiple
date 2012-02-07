9e758c1a4317e7c9038d6b3dc35e9e2915a5607a
cezaryrk <cezaryrk@361063.no-reply.drupal.org>
2012-02-07 22:36:19 +0100

    update template function

--- field-group-multiple-container.tpl.php
+++ field-group-multiple-container.tpl.php
@@ -4,31 +4,23 @@
  * - group
  * - items
  *  - delta + field_name + value render_array or null if empty
+ *
  */
 ?>
-<div class="field-group-mutliple-container clearfix field-label-inline">
-
-<div class="field-items field-group-multiple-items field-group-multiple-horizontal-items">
-<?php foreach($items as $delta => $item): ?>
-
-<div class="field-group-multiple-item <?= $delta % 2 == 1 ? "odd" : "even" ?>">
-<?php foreach($item as $field_name => $field_value): ?>
-
-<div class="field-item <?= $field_name ?>">
-<?php if(!is_null($field_value)): ?>
-   <?= render($field_value); ?>
-<?php else: ?>
-  <span class="field-is-empty"> </span>
-<?php endif; ?>
-</div>
-
-
-<?php endforeach; ?>
-</div>
-
-
-
-<?php endforeach; ?>
-
-</div>
+<div class="field-group-multiple-container clearfix">
+  <div class="field-group-multiple-items">
+  <?php foreach($entries as $delta => $entry): ?>
+    <div class="multiple-inline-element <?= $delta % 2 == 1 ? "odd" : "even" ?> clearfix">
+    <?php foreach($entry as $field_name => $field): ?>
+      <div class="multiple-element field-item-<?= $field_name ?>">
+      <?php if(!is_null($field)): ?>
+        <?= render($field); ?>
+      <?php else: ?>
+        <span class="field-is-empty"> </span>
+      <?php endif; ?>
+      </div>
+    <?php endforeach; ?>
+    </div>
+  <?php endforeach; ?>
+  </div>
 </div>
--- field-group-multiple-label-value.tpl.php
+++ field-group-multiple-label-value.tpl.php
@@ -1,34 +1,22 @@
 <?php
 /**
+ * TODO documantation
  *
  * - group
  * - items
  *  - delta + field_name + value render_array or null if empty
  */
 ?>
-<div class="field-group-multiple-label-value clearfix">
-
-<div class="field-items field-group-multiple-items field-group-multiple-horizontal-items">
-<?php foreach($items as $delta => $item): ?>
-
-<div class="field-group-multiple-item <?= $delta % 2 == 1 ? "odd" : "even" ?>">
-<?php foreach($item as $field_name => $field_value): ?>
-
-<div class="field-item <?= $field_name ?>">
-<?php if(!is_null($field_value)): ?>
-   <?= render($field_value); ?>
-<?php else: ?>
-  <span class="field-is-empty"> </span>
-<?php endif; ?>
-</div>
-
-
-<?php endforeach; ?>
-</div>
-
-
-
-<?php endforeach; ?>
-
-</div>
+<div class="field-group-multiple field-group-multiple-label-value clearfix">
+  <div class="field-group-multiple-items">
+  <?php foreach($entries as $delta => $entry): ?>
+    <div class="field-group-multiple-item <?= $delta % 2 == 1 ? "odd" : "even" ?>">
+    <?php if(!is_null($entry)): ?>
+      <?= render($entry); ?>
+    <?php else: ?>
+      <span class="field-is-empty"> </span>
+    <?php endif; ?>
+    </div>
+  <?php endforeach; ?>
+  </div>
 </div>
