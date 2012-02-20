49f1be2ebb81e0302cf5bc3dee22859240c96c03
cezaryrk <cezaryrk@361063.no-reply.drupal.org>
2012-02-20 13:22:13 +0100

    change <?= to <?php in templates and add empty check

--- field-group-multiple-container.tpl.php
+++ field-group-multiple-container.tpl.php
@@ -8,11 +8,11 @@
 <div class="field-group-multiple-container clearfix">
   <div class="field-group-multiple-items">
   <?php foreach($entries as $delta => $entry): ?>
-    <div class="multiple-inline-element <?= $delta % 2 == 1 ? "odd" : "even" ?> clearfix">
+    <div class="multiple-inline-element <?php print ($delta % 2 == 1 ? "odd" : "even"); ?> clearfix">
     <?php foreach($entry as $field_name => $field): ?>
-      <div class="multiple-element field-item-<?= $field_name ?>">
+      <div class="multiple-element field-item-<?php print $field_name; ?>">
       <?php if(!is_null($field)): ?>
-        <?= render($field); ?>
+        <?php print render($field); ?>
       <?php else: ?>
         <span class="field-is-empty"> </span>
       <?php endif; ?>
